8febdf01f66557623cfca7301b1dd9ae51098068
xavtry <sebastianishot68@gmail.com>
2025-11-15 14:55:17 -0500

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,1 +1,20 @@
+const { contextBridge, ipcRenderer } = require('electron');
 
+// Expose a secure API to the renderer process
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Renderer to Main (Send)
+  setTitle: (title) => {
+    ipcRenderer.send('set-title', title);
+  },
+
+  // Main to Renderer (Listen)
+  onNewTab: (callback) => {
+    ipcRenderer.on('shortcut-new-tab', (event, ...args) => callback(...args));
+  },
+  onCloseTab: (callback) => {
+    ipcRenderer.on('shortcut-close-tab', (event, ...args) => callback(...args));
+  },
+  onFocusURL: (callback) => {
+    ipcRenderer.on('shortcut-focus-url', (event, ...args) => callback(...args));
+  },
+});
